98cbf30a5ec6d0a708325a90be74f054608fb4a3
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-10-31 11:32:34 +0900

    second-commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,45 @@ $('a[href^="#"]').click(function () {
   $("body,html").animate({ scrollTop: position }, speed, "swing");
   return false;
 });
+
+// ハンバーガーメニュー
+$(function () {
+  $('.js-burger-btn').click(function () {
+    $(this).toggleClass('active')
+
+    if ($(this).hasClass('active')) {
+      $('.js-burger-menu').addClass('active')
+    } else {
+      $('.js-burger-menu').removeClass('active')
+    }
+  })
+})
+// ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
+$('#menu a[href]').on('click', function (event) {
+  $('.js-burger-btn').trigger('click')
+})
+// ×ボタン押したらハンバーガーメニューが閉じるように
+$('.js-burger-menu').on('click', function (event) {
+  $('.js-burger-btn').trigger('click')
+})
+
+$(function () {
+  $("#js-p-index-story__slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    speed: 3000,
+    responsive: [
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ],
+  });
+});
+
+
